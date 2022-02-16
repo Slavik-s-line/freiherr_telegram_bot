d0341d254c495111e3f93d4e6c44fae3f297234e
Viacheslav <78733510+Slavik-s-line@users.noreply.github.com>
2022-02-16 02:11:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import logging
-
+import requests
+import datetime
 from aiogram import Bot, Dispatcher, executor, types
 
+weather_token = "6e8d79779a0c362f14c60a1c7f363e29"
 API_TOKEN = 'BOT TOKEN HERE'
 
 # Configure logging
@@ -17,15 +19,26 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    await message.reply("Привет!\nНапиши мне название города и я сообщу погоду там!\n")
 
 
 @dp.message_handler()
-async def echo(message: types.Message):
-    # old style:
-    #await bot.send_message(message.chat.id, message.text)
-
-    await message.answer(message.text)
+async def get_weather(message: types.Message):
+    try:
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={weather_token}&units=metric")
+        data = r.json()
+        city = data["name"]
+        temperature = round(data["main"]["temp"])
+        humidity = round(data["main"]["humidity"])
+        pressure = round(data["main"]["pressure"])
+        wind = round(data["wind"]["speed"])
+        await message.reply(f"***{datetime.datetime.now().strftime('%b %d %Y %H:%M')}***\n"
+                            f"Погода в городе {city}\nТемпература {temperature} C°\n"
+                            f"Влажность: {humidity} %\nДавление: {pressure} мм.рт.ст.\n"
+                            f"Ветер: {wind} м/с\n ")
+    except:
+        await message.reply("\U00002620 Проверьте название города \U00002620")
 
 
 if __name__ == '__main__':
