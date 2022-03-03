4e70ec48a738c47b70d99a8d2cf117bb22ebabe0
Viacheslav <78733510+Slavik-s-line@users.noreply.github.com>
2022-03-03 15:54:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,10 @@ async def send_welcome(message: types.Message):
     """
     await message.reply("Привіт!\n")
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = ["Погода\U0001F30D", "Курс UAH\U0001F3E6", "Офіційні джерела\U00002139"]
-    keyboard.add(*buttons)
+    buttons_row1 = ["Погода\U0001F30D", "Курс UAH\U0001F3E6"]
+    buttons_row2 = ["Covid-19\U0001f9a0", "Офіційні джерела\U00002139"]
+    keyboard.add(*buttons_row1)
+    keyboard.add(*buttons_row2)
     await message.answer("Обери одну з функцій внизу: ", reply_markup=keyboard)
 
 
@@ -33,6 +35,7 @@ async def name_city(message: types.Message):
     await message.reply("Введіть назву міста: ")
 
     @dp.message_handler(lambda message: message.text != "Курс UAH\U0001F3E6"
+                                        and message.text != "Covid-19\U0001f9a0"
                                         and message.text != "Офіційні джерела\U00002139")
     async def without_puree(message: types.Message):
         try:
@@ -62,6 +65,19 @@ async def name_city(message: types.Message):
     await message.reply(f"\U0001F4B5 USD:\n НБУ: {nby[0].text[1:8]} Купівля: {buy[1].text[1:8]} Продаж:{sell[1].text[14:20]}\n\n"
                         f"\U0001F4B6 EUR:\n НБУ: {nby[1].text[1:8]} Купівля: {buy[3].text[1:8]} Продаж:{sell[3].text[14:20]}\n\n"
                         f"\U000020BD RUB:\n НБУ: {nby[2].text[1:8]} Купівля: {buy[5].text[1:7]} Продаж:{sell[5].text[13:19]}")
+
+
+@dp.message_handler(lambda message: message.text == "Covid-19\U0001f9a0")
+async def name_city(message: types.Message):
+    url = 'https://index.minfin.com.ua/ua/reference/coronavirus/geography/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    info = soup.find_all('td', class_='bg-total')
+    await message.reply(f"Статистака Covid-19 у світі на сьогодні: "
+                        f"{datetime.datetime.now().strftime('%b %d %Y %H:%M')}\n\n"
+                        f"\U0001f637Захворіли: {info[2].text}\n"
+                        f"\U0001f600Одужали: {info[6].text}\n"
+                        f"\U0001f494Померли: {info[4].text}\n")
 
 
 @dp.message_handler(lambda message: message.text == "Офіційні джерела\U00002139")
